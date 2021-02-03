c4a14d9c3564f7e49de7bf1054a6c180622696a2
Anna Zocher <anna.zocher@gmail.com>
2021-02-03 10:56:04 -0800

    fixed bugs

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Ingredient extends Component {
     render() {
         return (
-            <li backgroundColor={this.props.color} key={this.props.key}>
+            <li style={{backgroundColor: this.props.color}} key={this.props.key}>
                 {this.props.ingredient}
             </li>
         )
--- src/IngredientList.js
+++ src/IngredientList.js
@@ -4,9 +4,9 @@ import Ingredient from './Ingredient'
 class IngredientList extends Component {
     render() {
         // take array of props and use .map
-        let ingredientComponents = this.props.ingredients.map((item, index) => {
-            <Ingredient ingredient={item.name} color={item.color} key={key`${index}`} />
-        })
+        let ingredientComponents = this.props.ingredients.map((item, index) => (
+            <Ingredient ingredient={item.name} color={item.color} key={`key${index}`} />
+        ))
 
         return (
             <ul>
